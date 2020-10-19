39f8f68a1a20badac3b84dc9cd2df647d4a63289
Vishvak Seenichamy <vishvak365@gmail.com>
2020-10-18 22:49:54 -0400

    Added tutorial section

--- mainmenu.py
+++ mainmenu.py
@@ -1,7 +1,8 @@
 # This is a sample Python script.
-import pygame, sys
+import pygame
+import sys
 from pygame import *
-#pygame.init()
+# pygame.init()
 pygame.font.init()
 #size = (700, 500)
 #pygame.display.set_caption("My Game")
@@ -10,22 +11,26 @@ title = pygame.font.SysFont(None, 36)
 track = pygame.image.load('images/track.png')
 track = transform.scale(track, (100, 100))
 # Define some colors
-BLACK = ( 0, 0, 0)
-WHITE = ( 255, 255, 255)
-GREEN = ( 0, 255, 0)
-RED = ( 255, 0, 0)
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
 YELLOW = (255, 211, 0)
 # The loop will carry on until the user exit the game (e.g. clicks the close button).
 
 
 # The clock will be used to control how fast the screen updates
 clock = pygame.time.Clock()
+
+
 def text(text, font, color, surface, x, y):
     textobj = title.render(text, 1, color)
     textrec = textobj.get_rect()
     textrec.topleft = (x, y)
     surface.blit(textobj, textrec)
 # -------- Main Program Loop -----------
+
+
 def main_menu(screen):
     carryOn = True
     while carryOn:
@@ -34,8 +39,10 @@ def main_menu(screen):
         mx, my = pygame.mouse.get_pos()
         button1col = RED
         button2col = RED
+        button3col = RED
         button1 = pygame.Rect(50, 100, 200, 50)
         button2 = pygame.Rect(50, 200, 200, 50)
+        button3 = pygame.Rect(50, 300, 200, 50)
 
         if button1.collidepoint((mx, my)):
             button1col = YELLOW
@@ -46,12 +53,20 @@ def main_menu(screen):
             button2col = YELLOW
             if click:
                 options(screen)
+
+        if button3.collidepoint((mx, my)):
+            button3col = YELLOW
+            if click:
+                tutorial(screen)
+
         screen.fill(BLACK)
         text('Race Menu', title, YELLOW, screen, 310, 20)
         pygame.draw.rect(screen, button1col, button1)
         pygame.draw.rect(screen, button2col, button2)
+        pygame.draw.rect(screen, button3col, button3)
         text('Time Trial', font, BLACK, screen, 80, 120)
         text('Options', font, BLACK, screen, 80, 220)
+        text('Tutorial', font, BLACK, screen, 80, 320)
         click = False
         for event in pygame.event.get():  # User did something
             if event.type == pygame.QUIT:  # If user clicked close
@@ -66,6 +81,8 @@ def main_menu(screen):
         pygame.display.update()
         clock.tick(60)
     # once main loop exits
+
+
 def pick_track(screen):
     click = False
     in_opts = True
@@ -83,7 +100,7 @@ def pick_track(screen):
                 if event.button == 1:
                     click = True
 
-        trackCollide = pygame.Rect(40, 70, 120, 120);
+        trackCollide = pygame.Rect(40, 70, 120, 120)
         track_select_col = BLACK
         if trackCollide.collidepoint((mx, my)):
             track_select_col = YELLOW
@@ -97,10 +114,30 @@ def pick_track(screen):
         clock.tick(60)
     return False
 
+
 def options(screen):
     in_opts = True
     screen.fill(BLACK)
     text('Options', title, YELLOW, screen, 20, 20)
+    while in_opts:
+        for event in pygame.event.get():  # User did something
+            if event.type == pygame.QUIT:  # If user clicked close
+                pygame.quit()  # Flag that we are done so we exit this loop
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    in_opts = False
+
+        pygame.display.update()
+
+        clock.tick(60)
+
+
+def tutorial(screen):
+    in_opts = True
+    screen.fill(BLACK)
+    text('Tutorial', title, YELLOW, screen, 20, 20)
+    image = pygame.image.load('images/tutorial.png')
+    screen.blit(image,(0,100))
     while in_opts:
         for event in pygame.event.get():  # User did something
             if event.type == pygame.QUIT:  # If user clicked close
