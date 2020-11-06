687a564ed5d9685cc6fee558cd5088f3be71ba15
wboyd600 <37971619+wboyd600@users.noreply.github.com>
2020-11-05 23:03:56 -0500

    added innner/ outer track components to track class

--- track.py
+++ track.py
@@ -12,20 +12,28 @@ class Track:
     # Constructor calls static function to make track, track is list of extended sprite class objects
     def __init__(self):
         self.pads = []
-        self.pads = self.makePads()
         self.finish_line = (960, 50, 20, 125)
+        # inner and outer track set in makePads
+        self.inner_track = []
+        self.outer_track = []
+        self.pads = self.makePads()
 
     def update(self):
         pass
 
     def getFinishLine(self):
         return self.finish_line
+
+    def getInnerTrack(self):
+        return self.inner_track
+
+    def getOuterTrack(self):
+        return self.outer_track
+
     # Static method creates oval track using sin, cos
-    @staticmethod
-    def makePads():
+    def makePads(self):
         center_x = 1980 / 2
         center_y = 1020 / 2
-        width = 50
         pads = []
         for r in range(4):
             for i in range(int(360)):
@@ -35,6 +43,10 @@ class Track:
                 pad_y = math.floor(
                     (math.sin(radian) * (350 + 32 * r) + center_y))
                 pads.append(RoadSquares((pad_x, pad_y)))
+                if r == 1:
+                    self.inner_track.append((pad_x, pad_y))
+                elif r == 4:
+                    self.outer_track.append((pad_x, pad_y))
 
         return pygame.sprite.RenderPlain(*pads)
 
