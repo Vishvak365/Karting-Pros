55939cb4598ec863b85c8d29661e2b060c0e99d7
Vishvak Seenichamy <vishvak365@gmail.com>
2020-11-10 14:52:14 -0500

    Basic working json and reading

---
+++ settings.py
@@ -0,0 +1,19 @@
+import json
+JSON_FILE_NAME = "settings.json"
+
+
+def getMaxForwardSpeed():
+    f = open(JSON_FILE_NAME)
+    data = json.load(f)
+    print(data["max_forward_speed"])
+    f.close()
+
+
+def getSetting(settingName):
+    f = open(JSON_FILE_NAME)
+    data = json.load(f)
+    print(data[settingName])
+    f.close()
+
+
+getMaxForwardSpeed()
